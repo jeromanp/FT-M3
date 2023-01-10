36a8c5fab23e7f623eef17a8c60a2afb21148076
jeromanp <jeduardo.roman69@gmail.com>
2023-01-10 12:41:16 -0600

    Tarea Node terminada

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -1,7 +1,26 @@
-const process = require("process");
+const process = require("process"); // nativo de node
 const commands = require("./commands/index.js");
 
+function print(output){
+  process.stdout.write(output)
+  process.stdout.write("\nprompt > ")
+}
+
 function bash() {
+  process.stdout.write("prompt > ")
+  process.stdin.on("data", (data)=>{
+    const args = data.toString().trim().split(" ")
+    var cmd = args.shift() // cmd -> 'cd'    args -> [ 'commands', 'otra' ] <- join " "  'commands otra'
+    // console.log("--->", args)
+    // if(commands[cmd]){
+    if(commands.hasOwnProperty(cmd)){
+      commands[cmd](print, args.join(" ")) // commands.pwd(print, 'commands otra' )
+    } else {
+      print(`command not found: ${cmd}`)
+    }
+    // print(args)
+  })
+
 }
 
 bash();
--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -2,22 +2,59 @@ const fs = require("fs");
 const utils = require("../utils/request");
 const process = require("process");
 
-function pwd() {
-    
+
+function pwd(print) {
+  print(process.cwd());
 }
 
-function date() {}
+function date(print) {
+  print(Date());
+}
 
-function echo() {}
+function echo(print, args) {
+  print(args);
+}
 
-function ls() {}
+function ls(print) {
+  fs.readdir(".", function (error, files) {
+    if (error) throw new Error(error);
+    // console.log(files)
+    print(files.join(" "));
+  });
+}
 
-function cat() {}
+function cat(print, args) {
+  fs.readFile(args, "utf-8", (error, data) => {
+    if (error) throw new Error(error);
+    print(data);
+  }); // <23 43 54 etc...>
+}
 
-function head() {}
+function head(print, args) {
+  fs.readFile(args, "utf-8", (error, data) => {
+    if (error) throw new Error(error);
+    // console.log("data----------->", data)
+    const lines = data.split("\n")
+    print(lines[0].trim());
+  });
+}
 
-function tail() {}
+function tail(print, args) {
+  fs.readFile(args, "utf-8", (error, data) => {
+    if (error) throw new Error(error);
+    const lines = data.split("\n")
+    print(lines[lines.length-1].trim());
+    // print(lines.slice(-1)[0].trim());
+  });
+}
 
-function curl() {}
+function curl(print, args) {
+    utils.request(args, (error,response)=>{
+        if (error) throw new Error(error);
+        // console.log("wwwwwwwwww---->",response)
+        // print(response.data) // solo body
+        print(response)
+    })
+}
 
-module.exports = {};
+module.exports = { pwd, date, echo, ls, cat, head, tail, curl };
